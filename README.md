2add923db6b3728f91be496100cf04074395770c
Luca-coder07 <lucarandrianirina2507@gmail.com>
2025-07-20 21:21:21 +0300

    Camera et ajout de limite pour le joueur

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Escape-the-forrest
 Jeu entièrement créé avec Python Pygame
+
+
+Le but c'est de s'echapper des méchants dans la forêt et trouver la sortie pour terminer le jeu.
+
--- main.py
+++ main.py
@@ -4,30 +4,53 @@ import random as rd
 pg.init()
 
 screen = pg.display.set_mode((500, 500))
+
+# on ajoute la taille du monde en géneral
+world_width = 1000
+world_height = 1000
+
 tree = pg.image.load("assets/images/tree.png")
-perso = pg.image.load("assets/images/Brick.png")
+# On ajoute des changements de direction au personnage
+perso = [
+    pg.image.load("assets/images/player_right.png"),
+    pg.image.load("assets/images/player_left.png"),
+    pg.image.load("assets/images/player_up.png"),
+    pg.image.load("assets/images/player_down.png")
+]
+current_perso = perso[3] # On le met en face par defaut
 
 # on crée une liste qui va contenir tous les arbres
 forrest = []
-tree_nbr = rd.randint(10, 20)
+tree_nbr = rd.randint(30, 50)
 
 for i in range (tree_nbr):
     # On choisit la position de chaque arbre au hasard.
-    pos = [rd.randint(0, 400), rd.randint(0, 400)] 
+    pos = [rd.randint(0, 950), rd.randint(0, 950)] 
     forrest.append(pos)
 
 pg.display.set_caption("Escape the forrest")
 
 # Taille et position initiale du personnage
-perso_largeur = 100
-perso_hauteur = 100
+perso_largeur = 32
+perso_hauteur = 32
 perso_x = 225
 perso_y = 225
-vitesse = 0.3
+vitesse = 1
 
 #boucle principal du jeu
 running = 1
 while running:
+    # On calcuse l'offset du camera (au moitié de l'écran)
+    camera_x = perso_x - 250
+    camera_y = perso_y - 250
+
+    # On limite le camera pour qu'il n'aille pas en dehors du monde
+    camera_x = max(0, min(camera_x, world_width - 500))
+    camera_y = max(0, min(camera_y, world_height - 500))
+
+    # Pour que le joueur atteint le bord du monde
+    player_screen_x = perso_x - camera_x
+    player_screen_y = perso_y - camera_y
     for event in pg.event.get():
         if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
             running = 0
@@ -35,18 +58,27 @@ while running:
     # Deplacement
     touch = pg.key.get_pressed()
     if touch[pg.K_LEFT]:
-        perso_x -= vitesse
+        current_perso = perso[1]
+        if player_screen_x >= 0:
+            perso_x -= vitesse
     if touch[pg.K_RIGHT]:
-        perso_x += vitesse
+        current_perso = perso[0]
+        if player_screen_x <= 500 - 32: # Car le point a traiter est le point plus haut à gauche de l'image
+            perso_x += vitesse
     if touch[pg.K_DOWN]:
-        perso_y += vitesse
+        current_perso = perso[3]
+        if player_screen_y <= 500 - 32: # Car le point a traiter est le point plus haut à gauche de l'image
+            perso_y += vitesse
     if touch[pg.K_UP]:
-        perso_y -= vitesse
+        current_perso = perso[2]
+        if player_screen_y >= 0:
+            perso_y -= vitesse
+    
 
     screen.fill((110, 55, 10))
     for i in range (tree_nbr):
-        screen.blit(tree, (forrest[i][0], forrest[i][1]))
-    screen.blit(perso, (perso_x, perso_y))
+        screen.blit(tree, (forrest[i][0] - camera_x, forrest[i][1] - camera_y)) # On affiche les arbres dans des positions random
+    screen.blit(current_perso, (player_screen_x, player_screen_y)) # affichage du personnage au centre de l'ecran si on a pas atteint la position limite de l'ecran
     pg.display.update()
     pg.display.flip()
 
